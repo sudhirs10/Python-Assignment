5c62efb98708cdd820407f132da402a611b8fcf1
sudhirs10 <Sudhir.Shrestha2@metropolia.fi>
2025-09-04 16:31:55 +0545

    Add files via upload

---
+++ Assignment week2.py
@@ -0,0 +1,41 @@
+#Number 2
+#2.1
+# text=input('What is your name? ')
+#print("Hello, "+ text + "!")
+# text=input("What is your last name?\n")
+# print("Hello, "+ text + "")
+#2.2
+# import math
+# radius=int(input("Enter the radius of circle\n"))
+# print(math.pi*radius*radius)
+#3
+# length = int(input("Enter the length of Rectangle\n"))
+# width = int(input("Enter the width of Rectangle\n"))
+# perimeter = 2 * (length + width)
+# print("The perimeter of Rectangle is", perimeter)
+# area = length * width
+# print("The area of Rectangle is", area)
+#4
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# num3 = int(input("Enter the third number: "))
+# sumresult = num1 + num2 + num3
+# productresult = num1 * num2 * num3
+# averageresult = sumresult / 3
+# print("The sum is", sumresult)
+# print("The product is", productresult)
+# print("The average is", averageresult)
+#5
+# talents = int(input("Enter talents: "))
+# pounds = int(input("Enter pounds: "))
+# grams = totallots * 13.3
+# kilograms = int(grams // 1000)
+# remaining_grams = grams % 1000
+# print("The weight in modern units:")
+# print(kilograms, "kilograms and", remaining_grams, "grams")
+# 6
+# import random
+# code1 = str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+# code2 = str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6))
+# print("3 digit lock code:", code1)
+# print("4 digit lock code:", code2)
---
+++ Assignment1.py
@@ -0,0 +1,3 @@
+text=input("What is your name?\n")
+text1=input("What is your last name?\n")
+print("Hello,"+ " "+text +" "+ text1 +" ")
