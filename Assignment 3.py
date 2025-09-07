9f4f803d245d5aebf44fc4409f59055a49c8044b
sudhirs10 <Sudhir.Shrestha2@metropolia.fi>
2025-09-07 17:44:42 +0545

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,51 @@
+Module 3
+# 1
+# length = int(input("Enter the length of the zander in centimeter: "))
+# if length < 42:
+#     sizelimit = 42 - length
+#     print("Release the fish back into the lake!")
+#     print("The fish was", sizelimit, "cm below the size limit.")
+# else:
+#     print("Good! The fish meets the size limit.")
+
+# 2
+# cabin = input("Enter the cabin class LUX, A, B, C: ")
+# if cabin == "LUX":
+#     print("Upper-deck cabin with a balcony.")
+# elif cabin == "A":
+#     print("Above the car deck, equipped with a window.")
+# elif cabin == "B":
+#     print("Windowless cabin above the car deck.")
+# elif cabin == "C":
+#     print("Windowless cabin below the car deck.")
+# else:
+#     print("Invalid cabin class.")
+
+# 3
+# gender = input("Enter gender: ")
+# h = int(input("Enter hemoglobin value (g/l): "))
+# if gender == "female":
+#     if h < 117:
+#         print("Hemoglobin is low")
+#     elif h > 155:
+#         print("Hemoglobin is high")
+#     else:
+#         print("Hemoglobin is normal")
+# elif gender == "male":
+#     if h < 134:
+#         print("Hemoglobin is low")
+#     elif h > 167:
+#         print("Hemoglobin is high")
+#     else:
+#         print("Hemoglobin is normal")
+
+# 4
+# year = int(input("Enter a year: "))
+# if year % 4 != 0:
+#     print("not a leap year")
+# elif year % 100 != 0:
+#     print("leap year")
+# elif year % 400 == 0:
+#     print("leap year")
+# else:
+#     print("not a leap year")
