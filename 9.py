51ae2cde4259f97c2e701c033e99b4ded7253811
sudhirs10 <Sudhir.Shrestha2@metropolia.fi>
2025-10-28 03:05:06 +0545

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,113 @@
+# 1
+# Creates a car with speed and distance set to 0
+# Prints all car details
+class Car:
+    def __init__(self, registration_number, max_speed):
+        self.registration_number = registration_number
+        self.max_speed = max_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+my_car = Car("AB-123", 142)
+print(f"Registration number of Car is {my_car.registration_number}: The Max speed of car is {my_car.max_speed} km/h, Current speed {my_car.current_speed} km/h, Travelled distance {my_car.travelled_distance} km")
+
+# 2
+# Adding accelerate method to change car speed
+# Tests with +30, +70, +50 and then -200 for braking
+class Car:
+    def __init__(self, registration_number, max_speed):
+        self.registration_number = registration_number
+        self.max_speed = max_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+    def accelerate(self, change):
+        self.current_speed = self.current_speed + change
+        if self.current_speed > self.max_speed:
+            self.current_speed = self.max_speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+my_car = Car("AB-123", 142)
+my_car.accelerate(30)
+my_car.accelerate(70)
+my_car.accelerate(50)
+print(f"The current speed is: {my_car.current_speed} km/h")
+my_car.accelerate(-200)
+print(f"The final speed after braking is : {my_car.current_speed} km/h")
+
+
+# 3
+# Adding drive method to calculate distance using time and speed
+# Then prints all car detail
+class Car:
+    def __init__(self, registration_number, max_speed):
+        self.registration_number = registration_number
+        self.max_speed = max_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+    def accelerate(self, change):
+        self.current_speed = self.current_speed + change
+        if self.current_speed > self.max_speed:
+            self.current_speed = self.max_speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+
+    def drive(self, hours):
+        self.travelled_distance = self.travelled_distance + self.current_speed * hours
+my_car = Car("AB-123", 142)
+my_car.accelerate(60)
+my_car.drive(1.5)
+print("Car:", my_car.registration_number)
+print("Max speed of Car:", my_car.max_speed, "km/h")
+print("Current speed of Car:", my_car.current_speed, "km/h")
+print("Total Travelled distance:", my_car.travelled_distance, "km")
+
+# 4
+# make 10 cars and starts a race
+# speed changes every hour
+# race ends when one car reaches 10000 km
+# shows all cars and the winner
+
+import random
+class Car:
+    def __init__(self, registration_number, max_speed):
+        self.registration_number = registration_number
+        self.max_speed = max_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+    def accelerate(self, change):
+        self.current_speed = self.current_speed + change
+        if self.current_speed > self.max_speed:
+            self.current_speed = self.max_speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+    def drive(self, hours):
+        self.travelled_distance = self.travelled_distance + self.current_speed * hours
+cars = []
+for i in range(1, 11):
+    speed = random.randint(100, 200)
+    reg = "ABC-" + str(i)
+    car = Car(reg, speed)
+    cars.append(car)
+race_over = False
+hours = 0
+winner = None
+print("The race has started")
+while not race_over:
+    hours = hours + 1
+    for car in cars:
+        change = random.randint(-10, 15)
+        car.accelerate(change)
+        car.drive(1)
+        if car.travelled_distance >= 10000:
+            race_over = True
+            winner = car
+            break
+print()
+print("Race finished in", hours, "hours.")
+print("Registration", "MaxSpeed", "CurrentSpeed", "Distance")
+for car in cars:
+    print(car.registration_number, car.max_speed, car.current_speed, round(car.travelled_distance, 1))
+print()
+print("The winner is", winner.registration_number, "with", round(winner.travelled_distance, 1), "km!")
+print("Race over, congratulations to the winner!")
+
+
